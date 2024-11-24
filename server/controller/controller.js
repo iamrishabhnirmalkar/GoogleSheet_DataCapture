5e3018d697ad7cfb627d47ab7a7cc887b4500c09
Iamrishabhnirmalkar <iamrishabhnirmalkar@gmail.com>
2024-11-24 08:44:31 +0530

    fear: Controllersetup

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,17 +1,38 @@
 import { ZodError } from 'zod'
 import { enquirySchema } from '../helper/validator.js'
+import { sheets } from '../service/googleSheet.js'
+import appConfig from '../config/appConfig.js'
+import dayjs from 'dayjs'
+
+const { GOOGLE_SHEET_ID } = appConfig
 
 export const enquiryController = (req, res) => {
     try {
-        // 1.Body Validation
+        // 1. Body Validation
         const body = enquirySchema.parse(req.body)
         const { name, category, emailAddress, message } = body
 
-        // 2.Google Sheet Entry
+        const currentDate = dayjs().format('DD-MM-YYYY')
+        // 2. Google Sheet Entry
+        sheets.spreadsheets.values
+            .append({
+                spreadsheetId: GOOGLE_SHEET_ID,
+                range: 'Enquiry!A:E',
+                insertDataOption: 'INSERT_ROWS',
+                valueInputOption: 'RAW',
+                requestBody: {
+                    values: [[name, emailAddress, category, message, currentDate]]
+                }
+            })
+            .catch((err) => {
+                console.error(err.response.data.error)
+            })
 
         // 3. Response Send
-
-        res.sendStatus(200)
+        res.status(201).json({
+            success: true,
+            message: 'Success'
+        })
     } catch (err) {
         if (err instanceof ZodError) {
             return res.status(422).json({
@@ -19,6 +40,7 @@ export const enquiryController = (req, res) => {
                 message: err.errors
             })
         }
+
         res.sendStatus(500)
     }
 }
