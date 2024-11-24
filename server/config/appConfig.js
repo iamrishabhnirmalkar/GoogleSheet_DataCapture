a47acc64c8f40f4003207f86a693e55f50f8bbfd
Iamrishabhnirmalkar <iamrishabhnirmalkar@gmail.com>
2024-11-24 08:11:42 +0530

    feat: Configfile Setup

---
+++ server/config/appConfig.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default Object.freeze({
+    PORT: process.env.PORT,
+
+    // Google Sheet
+    GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
+    GOOGLE_SHEET_CLIENT_EMAIL: process.env.GOOGLE_SHEET_CLIENT_EMAIL,
+    GOOGLE_SHEET_PRIVATE_KEY: process.env.GOOGLE_SHEET_PRIVATE_KEY
+})
+
