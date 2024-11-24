2eddf1bb375bcfb2ff8f56e8719e50c20dcd0220
Iamrishabhnirmalkar <iamrishabhnirmalkar@gmail.com>
2024-11-24 09:26:17 +0530

    fix: Cors error fix

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import appConfig from './config/appConfig.js'
 import router from './router/router.js'
+import cors from 'cors'
 
 const { PORT } = appConfig
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/', router)
 
